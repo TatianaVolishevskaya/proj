a1eb104b42e5dd98b0cf9d39b27049afb4e9f41d
TatianaVolishevskaya <Volishevskaya.tatiana@gmail.com>
2024-11-27 00:09:05 +0300

    улучшили логику task

--- src/main/java/com/stepup/proj.java
+++ src/main/java/com/stepup/proj.java
@@ -4,12 +4,9 @@ public class proj {
     public static void main (String[] args) {
         System.out.println("Hello World");
         System.out.println("111111");
-        System.out.println("112211");
-        System.out.println("18591");
         System.out.println("1");
         System.out.println("0");
         System.out.println("55");
-        System.out.println("56");
     }
 
 }
