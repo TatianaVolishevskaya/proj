70050325fd171392d3b7c3ec2df49c91dc65a17e
TatianaVolishevskaya <Volishevskaya.tatiana@gmail.com>
2024-11-26 23:31:50 +0300

    изменили логику добавлен класс коробка

---
+++ src/main/java/com/stepup/box.java
@@ -0,0 +1,19 @@
+package com.stepup;
+
+public class box {
+    private int size;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+    public box() {
+
+    }
+    public box(int size) {
+        this.size = size;
+    }
+}
--- src/main/java/com/stepup/proj.java
+++ src/main/java/com/stepup/proj.java
@@ -4,5 +4,6 @@ public class proj {
     public static void main (String[] args) {
         System.out.println("Hello World");
         System.out.println("111111");
+        System.out.println("112211");
     }
 }
